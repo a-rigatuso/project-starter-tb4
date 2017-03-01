b54144447cb40fee579e56ba6767d7c0697d716c
Alexander Rigatuso <alex@rigatuso.de>
2017-03-01 16:30:36 +0100

    Add structure for a later Gulp/Grunt setup

--- config.rb
+++ config.rb
@@ -1,9 +1,9 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "scss"
-images_dir = "img"
-javascripts_dir = "js"
+css_dir = "DIST/css"
+sass_dir = "SRC/scss"
+images_dir = "SRC/img"
+javascripts_dir = "SRC/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
